0bba18972c70d1285002b3936ae1e4e445dda355
Eldar Eminov <em88el@gmail.com>
2024-01-22 22:27:51 +0300

    Small update styles

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[7.0].define(version: 2023_11_10_160032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "timescaledb"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
